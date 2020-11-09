071c083729dfffe0fcd8a735d7640d95bf5d92d7
Benjamin Buss <bendybuss@gmail.com>
2020-11-09 13:18:26 -0700

    Initial Commit
    Added README outline, as well as an early draft of the MP Crawler itself.

--- README.md
+++ README.md
@@ -1,1 +1,124 @@
-mountain_project_crawler
+# Mountain Project Tick Scraper
+
+Have you ever wanted a near unending amount of very niche data? Do you want to feel bad about pulling all this data from a site that provides a quality service for free? Well, I have just the script for you. This poorly made scraper is designed to take a mountain project area(E.g `https://www.mountainproject.com/area/105716826/saint-george`), and scrape all areas and subareas. Then parse and yield some basic route data, before finally, pulling all ticks for each area and parsing out all of their previous ticks.
+
+## Table of Contents
+
+* [About the Project](#about-the-project)
+* [Getting Started](#getting-started)
+  * [Prerequisites](#prerequisites)
+  * [Installation](#installation)
+* [Usage](#usage)
+  * [Data Format](#data-format)
+* [Roadmap](#roadmap)
+* [Contributing](#contributing)
+* [License](#license)
+* [Contact](#contact)
+* [Acknowledgements](#acknowledgements)
+
+
+
+
+## About The Project
+
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+This is an example of how to list things you need to use the software and how to install them.
+* npm
+```sh
+npm install npm@latest -g
+```
+
+### Installation
+
+1. Clone the repo
+```sh
+git clone https://github.com/BenjaminBuss/mountain_project_scraper.git
+```
+2. Install NPM packages
+```sh
+npm install
+```
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Use this space to show useful examples of how a project can be used. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.
+
+Before running the script, you need to set the `FEED` to your prefered export method. An Amazon S3 Bucket was originally used. For more information on Feed Exports check out the Scrapy documentation [here](https://docs.scrapy.org/en/latest/topics/feed-exports.html). 
+
+
+### Data Format
+
+Three different scrapy items are returned through the running of the script:
+
+1. **Route Data**
+
+Name | Type | Description
+---- | ---- | -----------
+route_id | *int* | Numerical identifier for the route pulled from the Mountain Project URL.
+route_name | *string* | The routes name.
+route_grade | *string* | The routes YDS or V-scale grade.
+
+
+2. **Tick Data**
+
+Name | Type | Description
+---- | ---- | -----------
+user_id | *int* | Numerical identifier for the user pulled from the MP URL.
+route_id | *int* | Numerical identifier for the route pulled from MP URL.
+route_type | *string* | Type(Boulder, Sport, Trad, Alpine).
+route_grade | *string* | The routes YDS or V-scale grade.
+route_notes | *string* | Any notes about send type(flash, onsight, attempt) as well as any additional information users provided.
+
+
+3. **Route Ticks**
+
+
+
+
+
+# All of this may go unused
+
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/github_username/repo_name/issues) for a list of proposed features (and known issues).
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Your Name - [@twitter_handle](https://twitter.com/twitter_handle) - email
+
+Project Link: [https://github.com/github_username/repo_name](https://github.com/github_username/repo_name)
+
+
---
+++ tickScraper.py
@@ -0,0 +1,150 @@
+# Benjamin Buss
+# October 25th 2020
+# Mountain Project Scraping idea draft
+
+
+import scrapy
+#from scrapy.crawler import CrawlerProcess
+#import numpy as np
+import re
+
+def strip_id(url):
+    temp_regex = re.findall(r'\d+', url)
+    temp_stripped = list(map(int, temp_regex))
+    return temp_stripped
+
+route_info = []  # np.array()
+area_codes = []  # np.array()
+route_codes = []  # np.array()
+user_codes = []  # np.array()
+
+class tickData(scrapy.Item):
+    user_id = scrapy.Field()
+    route_id = scrapy.Field()
+    route_type = scrapy.Field()
+    route_grade = scrapy.Field()
+    route_notes = scrapy.Field()
+
+class routeData(scrapy.Item):
+    route_id = scrapy.Field()
+    route_name = scrapy.Field()
+    route_grade = scrapy.Field()
+
+
+class ProjectSpider(scrapy.Spider):
+    name = 'mpScraper'
+    allowed_domains = ['mountainproject.com']
+    start_urls = ['https://www.mountainproject.com/area/118272520/wales-canyon']
+
+    route_info = []  # np.array()
+    area_codes = []  # np.array()
+    route_codes = []  # np.array()
+    user_codes = []  # np.array()
+
+    def parse(self, response):
+        # area_name = response.css('div.mp-sidebar a[href*=area] ::text').getall()
+        area_link = response.css('div.mp-sidebar a[href*=area]::attr(href)').getall()
+
+        if len(area_link) <= 0:
+            route_link = response.css('div.mp-sidebar a[href*=route]::attr(href)').getall()
+            for url in route_link:
+                if strip_id(url) in route_codes:
+                    break
+                else:
+                    route_codes.append(strip_id(url))
+                    yield response.follow(url=url, callback=self.parse_routes)
+        else:
+        # Iterate through subarea urls, add to an array, send them to be parsed
+            for url in area_link:
+                area_codes.append(strip_id(url))
+                yield response.follow(url=url, callback=self.parse_subareas)
+
+    def parse_subareas(self, response):
+        area_link = response.css('div.mp-sidebar a[href*=area]::attr(href)').getall()
+        route_link = response.css('div.mp-sidebar a[href*=route]::attr(href)').getall()
+
+        if len(area_link) == 0 and len(route_link) != 0:
+            for url in route_link:
+                if strip_id(url) in route_codes:
+                    break
+                else:
+                    route_codes.append(strip_id(url))
+                    yield response.follow(url=url, callback=self.parse_routes)
+        else:
+            for url in area_link:
+                if strip_id(url) in area_codes:
+                    break
+                else:
+                    area_codes.append(strip_id(url))
+                    yield response.follow(url=url, callback=self.parse_subareas)
+
+    def parse_routes(self, response):
+        route_name = response.css('div.col-md-9.float-md-right.mb-1 h1::text').get().strip()
+        route_grade = response.css("span.rateYDS::text").get()
+        #route_stars = response.css("[id='route-star-avg'] *::text").getall()  # needs some cleaning
+        # route_share = GET SHARED DATE?? IT'S IN A TABLE
+
+        route_subinfo = response.css('a.show-tooltip ::attr(href)').get()
+
+        route_id = int(strip_id(route_subinfo)[0])
+
+        route_info = routeData()
+        route_info['route_id'] = route_id
+        route_info['route_name'] = route_name
+        route_info['route_grade'] = route_grade
+
+        yield route_info
+
+        yield response.follow(url=route_subinfo, callback=self.get_users)
+
+    def get_users(self, response):
+        route_id = strip_id(response.url)
+        route_ticks = response.css('.col-lg-6  a[href*=user]::attr(href)').getall()
+
+        for url in route_ticks:
+            user_id = strip_id(url)
+
+            yield {'route_id': route_id[0], 'user_id': user_id}
+
+            if user_id in user_codes:
+                break
+            else:
+                user_codes.append(user_id)
+                yield response.follow(url=url, callback=self.open_user)
+
+    def open_user(self, response):
+        user_ticks = response.css('div.section-title a[href*=ticks]::attr(href)').getall()
+
+        for url in user_ticks:
+            yield response.follow(url=url, callback=self.parse_user)
+
+    def parse_user(self, response):
+        user_id = strip_id(response.url)
+        user_ticks = response.css('a[href*=route]::attr(href)').getall()
+        tick_grades = response.css('span.rateYDS::text').getall()
+        tick_type = response.css('span.small.text-warm.pl-half span:nth-child(2) ::text').getall()
+        tick_details = response.css('td.text-warm.small i ::text').getall()
+
+        pagination = response.css('div.pagination a:nth-child(4) ::attr(href)').get()
+
+        route_id = []
+
+        for i in user_ticks:
+            route_id.append(strip_id(i))
+
+        for item in zip(route_id, tick_type, tick_grades, tick_details):
+            tick = tickData()
+            tick['user_id'] = user_id
+            tick['route_id'] = item[0]
+            tick['route_type'] = item[1]
+            tick['route_grade'] = item[2]
+            tick['route_notes'] = item[3]
+
+            yield tick
+
+        if not pagination:
+            return
+        else:
+            yield response.follow(url=pagination, callback=self.parse_user)
+
+
